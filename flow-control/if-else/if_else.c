76cdcce354ba53469093bb8be36135daf5489a96
Nitin <nitin8285824551@gmail.com>
2022-03-30 01:08:04 +0530

    move intro files and folders into Introduction folder and create flow-control if-else program

---
+++ flow-control/if-else/if_else.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main(){
+
+    // // Example 1. Program to display a number if it is negative
+    // int a;
+    
+    // printf("Enter the number: ");
+    // scanf("%d", &a);
+    // if (a > 0){
+    //     printf("The Number is Positive");
+    // } else {
+    //     printf("The Number is Negative");
+    // }
+
+
+    // // Example 2. Check whether an integer is odd or even
+    // int a;
+    
+    // printf("Enter the number: ");
+    // scanf("%d", &a);
+    // if (a%2 == 0){
+    //     printf("The Number is Even");
+    // } else {
+    //     printf("The Number is Odd");
+    // }
+
+
+    /*
+        if..else ladder
+        The if...else statement executes two different codes 
+        depending upon whether the test expression is true or 
+        false. Sometimes, a choice has to be made from more 
+        than 2 possibilities.
+    */
+
+    // // Example 3. Program to relate two integers using =, >, < symbol
+    // int num1, num2;
+    
+    // printf("Enter the number: ");
+    // scanf("%d%d", &num1, &num2);
+    // if (num1 == num2){
+    //     printf("%d = %d",num1, num2);
+    // } else if (num1 > num2) {
+    //     printf("%d > %d",num1, num2);
+    // } else if (num1 < num2) {
+    //     printf("%d < %d",num1, num2);
+    // }
+
+    /*
+        Nested if..else 
+        This program given below relates two integers using either <, > 
+        and = similar to the if...else ladder's example. However, we will 
+        use a nested if...else statement to solve this problem.
+    */
+
+    // // If body of an if..else has only one statement, you do not 
+    // // need to use brackets {}.
+    // if (3 > 2)
+    // printf("True");
+
+    return 0;
+}
