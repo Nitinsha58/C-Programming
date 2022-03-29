d5ade881a0fc25bf23439263a12415f8ab6a13c7
Nitin <nitin8285824551@gmail.com>
2022-03-30 00:48:54 +0530

    C language introduction practice programs

---
+++ intro_example/example.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+
+int main(){
+    // //Example 1. Program to print a Sentence
+    // printf("Hello world");
+
+
+    // // Example 2. Program to print an integer entered by the user
+    // int integer;
+    // printf("Enter the number: ");
+    // scanf("%i", &integer);
+
+    // printf("The number is: %d", integer);
+
+
+    // // Example 3. Program to add two integers entered by the User
+    // int num1, num2, c;
+    // printf("Enter num1 and num2: ");
+    // scanf("%d%d", &num1, &num2);
+    // c = num1 + num2;
+    // printf("Sum of %d and %d is %d", num1, num2, c);
+
+
+    // // Example 4. Program to multiply two floating point numbers
+    // float num1, num2, c;
+    // printf("Enter num1 and num2: ");
+    // scanf("%f%f", &num1, &num2);
+    // c = num1 * num2;
+    // printf("Multiplication of %g and %g is %g", num1, num2, c);
+
+    
+    // // Example 5. Program to find ASCII value of a character entered by the user
+    // char chr;
+
+    // printf("Enter the character: ");
+    // scanf("%c", &chr);
+
+    // printf("The ASCII value of '%c' is %d: ", chr, chr);
+    
+
+    // // Example 6. Program to find quotient and remainder of Two Integers
+    // int num1, num2;
+
+    // printf("Enter the num1, and num2 in float: ");
+    // scanf("%d%d", &num1, &num2);
+    // int quotient = num1 / num2;
+    // int remainder = num1 % num2;
+
+    // printf("Remainder: %d, Quotient: %d", remainder, quotient);
+
+
+    // // Example 7. Program to find the size of int, float, double and char
+    // int a;
+    // float b;
+    // double c;
+    // char d;
+
+    // printf("size of a: %d\n", sizeof(a));
+    // printf("size of b: %d\n", sizeof(b));
+    // printf("size of c: %d\n", sizeof(c));
+    // printf("size of d: %d\n", sizeof(d));
+
+
+    // // Example 8. Program to demonstrate the working of keyword long 
+    // long a;
+    // a = 23233;
+    // printf("The number is: %d", a);
+
+
+    // // Example 9. Program to swap two numbers
+    // int a = 2, b = 3, c;
+
+    // printf("Before Swap: a is: %d, b is %d", a, b);
+    // c = a;
+    // a = b;
+    // b = c;
+    // printf("After Swap: a is: %d, b is %d", a, b);
+
+    return 0;
+}
