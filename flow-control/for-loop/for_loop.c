6a2f15e2c1302198bc8bc734724986b44e94547d
Nitin <nitin8285824551@gmail.com>
2022-03-30 01:38:19 +0530

    for loop example

---
+++ flow-control/for-loop/for_loop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    // // Example 1. Print numbers from 1 to 10
+    // for (int i = 1; i <= 10; ++i){
+    //     printf("%d ", i);
+    // }
+
+
+    // // Example 2. Program to calculate sum of first n natural numbers
+    // int num, sum = 0;
+
+    // printf("Enter the positive number: ");
+    // scanf("%d", &num);
+    
+    // for (int i = 1; i <= num; ++i){
+    //     sum += i;
+    // }
+    // printf("Sum of first %d nums is %d:", num, sum);
+
+    return 0;
+}
