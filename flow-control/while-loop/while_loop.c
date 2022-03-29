7573fea60602464375bba2e882a29033b1981c63
Nitin <nitin8285824551@gmail.com>
2022-03-30 01:56:58 +0530

    example of while loop and do while loop

---
+++ flow-control/while-loop/while_loop.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+    // While loop
+    // // Example 1. Program to print first 10 natural numbers
+    // int i = 1;
+
+    // while (i <= 10){
+    //     printf("%d\n", i);
+    //     ++i;
+    // }
+
+    // // do while loop
+    // double num, sum=0; 
+    // do {
+    //     printf("Enter a number: ");
+    //     scanf("%lf", &num);
+    //     sum += num;
+        
+    // } while (num != 0.0);
+    // printf("Sum is: %lf", sum);
+
+    return 0;
+}
